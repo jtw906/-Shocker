ab9214e0e53a9bada93ccfd4e5f964e44cdb693c
jtw906 <32492369+jtw906@users.noreply.github.com>
2018-05-24 17:21:12 -0500

    hjhuyu

---
+++ src/com/shocker/engine/GameContainer.java
@@ -0,0 +1,106 @@
+package com.shocker.engine;
+
+public class GameContainer implements Runnable{
+	private Thread thread;
+	private boolean running = false;
+	private final double UPDATE_CAP = 1.0/60.0;
+	private int width = 1600 ,height = 900;
+	private float scale = 1f;
+	private String title = "ShockerEngine v1.0";
+	private Window window;
+	private Renderer renderer;
+	public GameContainer(){
+		
+	}
+	public void start() {
+		window = new Window(this);
+		renderer = new Renderer(this);
+		thread = new Thread(this);
+		thread.run();
+	}
+	public void stop() {
+		
+	}
+	public void run() {
+		running = true;
+		boolean render = false;
+		double firstTime = 0;
+		double lastTime = System.nanoTime()/1000000000.0;
+		double passedTime = 0;
+		double unprocessedTime = 0;
+		double frameTime =0;
+		int frame = 0;
+		int fps = 0;
+		
+		while(running) {
+			render = false;
+			firstTime = System.nanoTime()/1000000000.0;
+			passedTime = firstTime - lastTime;
+			lastTime = firstTime;
+			
+			unprocessedTime += passedTime;
+			frameTime += passedTime;
+			while(unprocessedTime >= UPDATE_CAP) {
+				unprocessedTime -= UPDATE_CAP;
+				render = true;
+				//TODO: Update Game
+				if(frameTime>=1.0) {
+					frameTime=0;
+					fps = frame;
+					frame = 0;
+					
+				}
+			}
+			if(render){
+				//TODO: Render game
+				renderer.clear();
+				window.update();
+				frame++;
+				System.out.println("fps: " + fps);
+			}
+			else {
+				try {
+					Thread.sleep(1);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		dispose();
+	}
+
+	private void dispose() {
+		
+	}
+	public static void main(String args[]) {
+		GameContainer gc = new GameContainer();
+		gc.start();
+	}
+	public int getWidth() {
+		return width;
+	}
+	public void setWidth(int width) {
+		this.width = width;
+	}
+	public int getHeight() {
+		return height;
+	}
+	public void setHeight(int height) {
+		this.height = height;
+	}
+	public float getScale() {
+		return scale;
+	}
+	public void setScale(float scale) {
+		this.scale = scale;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public Window getWindow() {
+		return window;
+	}
+}
---
+++ src/com/shocker/engine/Input.java
@@ -0,0 +1,77 @@
+package com.shocker.engine;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+import java.awt.event.MouseWheelEvent;
+import java.awt.event.MouseWheelListener;
+
+public class Input implements KeyListener, MouseListener, MouseMotionListener, MouseWheelListener{
+
+	private final int NUM_KEYS = 256;
+	private boolean[] keys = new boolean[NUM_KEYS];
+	private boolean[] keysLast = new boolean[NUM_KEYS];
+	private final int NUM_BUTTONS= 5;
+	private boolean[] buttons = new boolean[NUM_BUTTONS];
+	private boolean[] buttonsLast = new boolean[NUM_BUTTONS];
+	public Input() {
+		
+	}
+
+	@Override
+	public void mouseWheelMoved(MouseWheelEvent e) {
+		
+	}
+
+	@Override
+	public void mouseDragged(MouseEvent e) {
+		
+	}
+
+	@Override
+	public void mouseMoved(MouseEvent e) {
+		
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		
+	}
+}
